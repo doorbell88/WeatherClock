80fee342a2ef94a6748af8106f5b2b2b38dec068
Kevin Klinkel <kgklinkel@gmail.com>
2018-01-19 22:46:21 +0000

    Got clock to work! Added .gitignore

--- pi_test.py
+++ pi_test.py
@@ -3,7 +3,7 @@ from neopixel import *
 #from pi_clock.py import *
 
 # LED strip configuration:
-LED_COUNT   = 1       # Number of LED pixels.
+LED_COUNT   = 24      # Number of LED pixels.
 LED_PIN     = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA     = 5       # DMA channel to use for generating signal (try 5)
@@ -15,6 +15,6 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
-color = Color(200, 100, 0)
+color = Color(5, 10, 0)
 strip.setPixelColor(0, color)
 strip.show()
