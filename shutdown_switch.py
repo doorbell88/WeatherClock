83ef15d328407c49ccfecdfc7fa9528dff03411c
Kevin Klinkel <kgklinkel@gmail.com>
2018-01-22 20:43:13 -0700

    Added shutdown_switch.py Added turn_off_LED_array.py

---
+++ shutdown_switch.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from gpiozero import Button, LED
+from signal import pause
+import os, sys
+import time
+
+offGPIO  = int(sys.argv[1]) if len(sys.argv) >= 2 else 21
+holdTime = int(sys.argv[2]) if len(sys.argv) >= 3 else 6
+ledGPIO  = int(sys.argv[3]) if len(sys.argv) >= 4 else 16
+
+def when_pressed():
+    # start blinking with 1/2 second rate
+    #led.blink(on_time=0.5, off_time=0.5)
+    print "pressed"
+
+def when_released():
+    # be sure to turn the LEDs off if we release early
+    #led.off()
+    print "released"
+
+def shutdown():
+    print "Yay!"
+    os.system("sudo python ~/WeatherClock/turn_off_LED_array.py")
+    time.sleep(1)
+    os.system("sudo poweroff")
+
+led = LED(ledGPIO)
+btn = Button(offGPIO, hold_time=holdTime)
+btn.when_held     = shutdown
+btn.when_pressed  = when_pressed
+btn.when_released = when_released
+pause()
