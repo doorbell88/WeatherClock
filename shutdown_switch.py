0a3bbde23b2ef1ab990c804af01d7c4dd65dd9a8
Kevin Klinkel <kgklinkel@gmail.com>
2018-01-26 12:16:26 -0700

    Corrected filename in shutdown_switch.py, and added comment lines

--- shutdown_switch.py
+++ shutdown_switch.py
@@ -5,6 +5,11 @@ import os, sys
 import time
 from config import BUTTON_PIN, BUTTON_HOLD_TIME, STATUS_LED_PIN
 
+#--------------------------------- VARIABLES -----------------------------------
+weather_clock_file = "weather_clock.py"
+
+
+#------------------------------- BUTTON ACTIONS --------------------------------
 def when_pressed():
     # start blinking with 1/2 second rate
     #led.blink(on_time=0.5, off_time=0.5)
@@ -17,14 +22,21 @@ def when_released():
 
 def shutdown():
     print "Yay!"
-    os.system("/home/pi/WeatherClock/kill_weather_clock.sh {}".format("pi_clock.py"))
+    os.system("/home/pi/WeatherClock/kill_weather_clock.sh {}".format(weather_clock_file))
     os.system("sudo python /home/pi/WeatherClock/turn_off_LED_array.py")
     time.sleep(1)
     os.system("sudo poweroff")
 
+
+#------------------------------------ MAIN -------------------------------------
+# Instantiate button and LED
 led = LED(STATUS_LED_PIN)
 btn = Button(BUTTON_PIN, hold_time=BUTTON_HOLD_TIME)
+
+# register button actions
 btn.when_held     = shutdown
 btn.when_pressed  = when_pressed
 btn.when_released = when_released
+
+# wait until a signal is received
 pause()
