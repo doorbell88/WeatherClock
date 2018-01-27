4a53323d145e2ddb5eb3faf7b7cdb2ecfa2fd9ff
Kevin Klinkel <kgklinkel@gmail.com>
2018-01-26 17:05:14 -0700

    Removed setting git pull at boot

--- add_to_run_at_boot.sh
+++ add_to_run_at_boot.sh
@@ -50,7 +50,6 @@ get_line_number "exit"
 insert_line "#-----------------------------------------------------------------"
 insert_line "# Scripts and commands used for WeatherClock"
 insert_line "#-----------------------------------------------------------------"
-insert_line '$(cd /home/pi/WeatherClock/ && git pull)'
 insert_line "/home/pi/WeatherClock/shutdown_switch.py &"
 insert_line "/home/pi/WeatherClock/weather_clock.py &"
 insert_line "#-----------------------------------------------------------------"
