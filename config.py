7f143379f95ea5a37e56280d44d2fd10ff2f957a
Kevin Klinkel <kgklinkel@gmail.com>
2018-03-31 17:18:08 -0600

    changed cursor colors (red=temp, violet=sky)

--- config.py
+++ config.py
@@ -41,8 +41,8 @@ ACTIVE_LEDS     = 12        # (If you have 24 LEDs, but only want one lit up for
 
 
 #------------------------------- CUSTOMIZATION ---------------------------------
-CURSOR_COLOR_SKY   = red        # Cursor color if weather is up-to-date
-CURSOR_COLOR_TEMP  = violet     # Cursor color if weather is up-to-date
+CURSOR_COLOR_SKY   = violet     # Cursor color if weather is up-to-date
+CURSOR_COLOR_TEMP  = red        # Cursor color if weather is up-to-date
 CURSOR_COLOR_API   = cyan       # Cursor color if weather is calling API
 CURSOR_COLOR_ERROR = black      # Cursor color if weather API call fails
 DISPLAY_MINUTE     = False      # True will display the minute hand. False won't
